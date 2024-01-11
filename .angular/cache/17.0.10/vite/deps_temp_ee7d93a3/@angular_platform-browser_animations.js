0790df2c968364fe791684edff13dad5672bd358
Акбархон Охунов <ohunovakbar565@gmail.com>
2024-01-11 22:32:18 +0500

    Добавьте файлы проекта.

---
+++ .angular/cache/17.0.10/vite/deps_temp_ee7d93a3/@angular_platform-browser_animations.js
@@ -0,0 +1,21 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-5BA3IFJT.js";
+import "./chunk-PWGVEUW6.js";
+import "./chunk-M3QTIEVZ.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-RFJ3ZG4R.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
